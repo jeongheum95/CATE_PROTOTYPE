c4060cd5c60604999bdf6b0c1d4ae238040a8502
jo <jeongheum95@gmail.com>
2019-06-06 11:50:35 +0900

    주소바꿈
    ㄴㄴ

--- YOUTUBE-API-master/app/src/main/java/app/com/youtubeapiv3/LoginRequest.java
+++ YOUTUBE-API-master/app/src/main/java/app/com/youtubeapiv3/LoginRequest.java
@@ -18,7 +18,7 @@ public class LoginRequest extends StringRequest {
     //현재 안드로이드앱을 에뮬레이터로 돌리므로 에뮬레이터가 설치된 서버에 있는 아파치 서버에 접근하려면
     //다음과 같이 10.0.2.2:포트번호 로 접근해야합니다 저는 8080 포트를 써서 다음과 같이 했습니다
 //    final static private String URL = "http://10.20.55.174/LLogin.php";
-    final static private String URL = "http://192.168.0.5/fow_loginReq.php";
+    final static private String URL = "https://catapro.000webhostapp.com//fow_loginReq.php";
     private Map<String, String> parameters;
 
     //생성자
