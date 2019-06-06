1027c4e0380d402d1605fc3ad43b968b31773951
ghkdua1829 <ghkdua1829@naver.com>
2019-06-06 12:11:11 +0900

    로그인 회원가입

---
+++ LoginRequest.java
@@ -0,0 +1,62 @@
+package com.form.gaip;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+
+public class LoginRequest extends StringRequest {
+
+    //현재 안드로이드앱을 에뮬레이터로 돌리므로 에뮬레이터가 설치된 서버에 있는 아파치 서버에 접근하려면
+    //다음과 같이 10.0.2.2:포트번호 로 접근해야합니다 저는 8080 포트를 써서 다음과 같이 했습니다
+//    final static private String URL = "https://pcbangq.000webhostapp.com/LLogin.php";
+    final static private String URL = "https://pcbangq.000webhostapp.com/LLogin.php";
+    private Map<String, String> parameters;
+
+    //생성자
+    public LoginRequest(String userID, String userPassword, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+
+
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("userPassword", userPassword);
+
+        TrustManager[] dummyTrustManager = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        } };
+
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, dummyTrustManager, new java.security.SecureRandom());
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //추후 사용을 위한 부분
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return parameters;
+    }
+}
+
+
